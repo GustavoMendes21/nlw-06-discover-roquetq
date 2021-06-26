4e939fab1b66e60c00512a82a66e29b9a72017be
Gustavo Mendes <gustavomendes2112@hotmail.com>
2021-06-25 21:15:12 -0300

    fourth-day

---
+++ src/route.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const QuestionController = require("./controller/QuestionController")
+const RoomController = require('./controller/RoomController')
+
+const route = express.Router()
+
+route.get("/", (req, res) => res.render("index", {page: 'enter-room'}))
+route.get("/create-pass", (req, res) => res.render("index", {page: 'create-pass'}))
+route.get("/room/:room", (req, res) => res.render("room"))
+
+route.post("/question/:room/:question/:action", QuestionController.index)
+route.post("/create-room", RoomController.create)
+
+module.exports = route
